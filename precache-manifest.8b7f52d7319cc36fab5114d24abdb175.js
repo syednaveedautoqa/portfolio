b27531a15ebc037105dec6c32a52a21ff1e9a157
syednaveed8 <syed.y.naveed@gmail.com>
2019-09-25 01:10:20 -0700

    Updates

---
+++ precache-manifest.8b7f52d7319cc36fab5114d24abdb175.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51fe3ad3aa9b592d79308fa3b64ed64d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7ba132feb03a93b92bc7",
+    "url": "/portfolio/static/css/main.883fedaa.chunk.css"
+  },
+  {
+    "revision": "f515d53716dbf6752e81",
+    "url": "/portfolio/static/js/2.65481a3d.chunk.js"
+  },
+  {
+    "revision": "7ba132feb03a93b92bc7",
+    "url": "/portfolio/static/js/main.7fe1c263.chunk.js"
+  },
+  {
+    "revision": "255cf37ac81f61801b31",
+    "url": "/portfolio/static/js/runtime~main.36963274.js"
+  },
+  {
+    "revision": "5acac418199f2923b93a4bb69e07ceb6",
+    "url": "/portfolio/static/media/Resume.5acac418.pdf"
+  },
+  {
+    "revision": "8262057119c963741b868e0d8dd68dd3",
+    "url": "/portfolio/static/media/economica-bold.82620571.ttf"
+  },
+  {
+    "revision": "56ab00e848077ea3fec713da56e69681",
+    "url": "/portfolio/static/media/linkedin_icon.56ab00e8.png"
+  },
+  {
+    "revision": "91070ad1182399798766037b1a3b24e8",
+    "url": "/portfolio/static/media/profile_pic.91070ad1.png"
+  },
+  {
+    "revision": "752169965d43ffdc21a1dae640da6c1f",
+    "url": "/portfolio/static/media/project1.75216996.png"
+  },
+  {
+    "revision": "06449e34c92f8856c901cc9939533519",
+    "url": "/portfolio/static/media/project4.06449e34.png"
+  },
+  {
+    "revision": "e0d0219f3a652b417922193d2827e84f",
+    "url": "/portfolio/static/media/project5.e0d0219f.png"
+  },
+  {
+    "revision": "cf4449c1884598b292630aa9f4007c19",
+    "url": "/portfolio/static/media/roboto-condensed.light.cf4449c1.ttf"
+  }
+]);
